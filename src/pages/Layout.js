31e72e6de10ea9485953869ac095bb45862540fa
Davin <davinrim10@gmail.com>
2023-05-27 16:47:01 +0900

    헤더, 로그인, 회원가입 화면 생성

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Layout = () => {
     return (
         <div className="container place-content-center">
-            <div className="header">헤더부분</div>
-
+            <Header />
             {/* 공통영역 밑에 들어가는 콘텐츠 */}
             <div id="content">
                 <Outlet />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
         container: {
             center: true,
         },
+        extend: {
+            colors: {
+                logo: '#FF0000',
+            },
+        },
     },
     plugins: [],
 };
