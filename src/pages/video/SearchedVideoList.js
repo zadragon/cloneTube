8be36d0865d87c517f4819b643c1472dd97b1d10
Davin <davinrim10@gmail.com>
2023-06-06 22:59:28 +0900

    검색창 수정

--- src/pages/video/SearchedVideoList.js
+++ src/pages/video/SearchedVideoList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Sidebar from '../../components/Sidebar';
 import VideoCard from '../../components/VideoCard';
 import { useCookies } from 'react-cookie';
-import { useMutation, useQuery } from 'react-query';
+import { QueryClient, useMutation, useQuery, useQueryClient } from 'react-query';
 import { apiVideo } from '../../api/api';
 import styled from 'styled-components';
 import MetaTag from '../../components/MetaTag';
@@ -16,30 +16,56 @@ export default function SearchedVideoList() {
 
     console.log(searchedVideoState);
 
-    // const {
-    //     data: dataSearch,
-    //     isLoading: isLoadingSearch,
-    //     error: errorSearch,
-    //     mutate: filteredvideos,
-    // } = useMutation(payload => {
-    //     return apiVideo.SearchResult(payload);
-    // });
+    const {
+        data: dataSearch,
+        isLoading: isLoadingSearch,
+        error: errorSearch,
+        mutate: filteredvideos,
+    } = useMutation(payload => {
+        return apiVideo.SearchResult(payload);
+    });
 
-    // const handleSubmit = e => {
-    //     e.preventDefault();
-    //     const payload = { search: search };
-    //     filteredvideos(payload, {
-    //         onSuccess: () => {},
-    //         onSuccess: () => {
-    //             console.log('dataSearch', dataSearch);
-    //         },
-    //     });
-    //     navigate(`/videos/${search}`);
-    // };
+    useEffect(() => {
+        filteredvideos({ search: searchedVideoState });
+    }, [searchedVideoState]);
 
-    // useEffect(() => {
-    //     console.log('dataSearch useEffect', dataSearch);
-    //     dispatch(SearchVideos({ dataSearch }));
-    // }, [dataSearch]);
-    return <></>;
+    const target = useRef();
+    return (
+        <>
+            <MetaTag
+                title="영상목록 :: LoneTube"
+                description="세계최대 동영상 서비스"
+                keywords="론튜브, 영상, 유튜브"
+            />
+            <div className="flex">
+                <Sidebar />
+                <div className="w-5/6 relative">
+                    <VideoWrap>
+                        {dataSearch?.data?.VideoList.map((videoInfo, idx) => {
+                            return <VideoCard key={idx} videoInfo={videoInfo} />;
+                        })}
+                    </VideoWrap>
+                    <InfyScrollTarget ref={target}></InfyScrollTarget>
+                </div>
+            </div>
+        </>
+    );
 }
+
+const VideoWrap = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    & > a {
+        width: 25%;
+    }
+`;
+
+const InfyScrollTarget = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100px;
+    height: 250px;
+    background-color: aliceblue;
+    opacity: 0.9;
+`;
